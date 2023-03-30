9b56effdc3e8286b7bb01bd994ad2ff4c0f21bda
Saikal <saikal-98@gmail.com>
2023-03-30 13:34:22 +0600

    added admin page

--- src/components/Product/AddProduct.jsx
+++ src/components/Product/AddProduct.jsx
@@ -1,65 +1,107 @@
 import React, { useState } from "react";
-import { Button, FormControl } from "react-bootstrap";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
-const AddBlog = ({ addCosmetics }) => {
-  const [title, setTitle] = useState("");
-  const [price, setPrice] = useState("");
-  const [description, setDescription] = useState("");
-  const [image, setImage] = useState("");
-
-  function handleValues() {
-    let newCosmetics = {
-      title,
-      price,
-      description,
-      image,
-    };
-    addCosmetics(newCosmetics);
-
-    console.log(newCosmetics);
-  }
+const AddProduct = () => {
+  const navigate = useNavigate();
+  const [product, setproduct] = useState({
+    name: "",
+    description: "",
+    price: "",
+    image: "",
+    type: "",
+  });
+  const handleInp = (e) => {
+    if (e.target.name == "price") {
+      let obj = {
+        ...product,
+        [e.target.name]: Number(e.target.value),
+      };
+      setproduct(obj);
+    } else {
+      let obj = {
+        ...product,
+        [e.target.name]: e.target.value,
+      };
+      setproduct(obj);
+    }
+  };
+  console.log(product);
   return (
-    <center>
-      <div className="">
-        <h2 className="my-5">ADD YOUR PRODUCT</h2>
-        <FormControl
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="w-50 my-2 mt-5 "
-          type="text"
-          placeholder="title"
-        />
-        <br />
-        <FormControl
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className="w-50 my-2 "
-          type="text"
-          placeholder="description"
-        />
-        <br />
-        <FormControl
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-          className="w-50 my-2 "
-          type="text"
-          placeholder="image"
-        />
-        <br />
-        <FormControl
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          className="w-50 my-2 "
-          type="text"
-          placeholder="price"
-        />
-
-        <Button onClick={handleValues} className="px-5 btn btn-dark fs-4">
-          Add
-        </Button>
-      </div>
-    </center>
+    <Box
+      sx={{
+        width: "40vw",
+        margin: "5vh auto",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        // background: "rgb(254, 249, 239)",
+        gap: "30px",
+        padding: "20px",
+      }}
+    >
+      <h1>ADMIN PAGE</h1>
+      <TextField
+        id="outlined-basic"
+        label="Name"
+        variant="outlined"
+        color="grey"
+        name="name"
+        size="small"
+        onChange={handleInp}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Description"
+        variant="outlined"
+        color="grey"
+        name="description"
+        size="small"
+        onChange={handleInp}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Price"
+        variant="outlined"
+        color="grey"
+        name="price"
+        size="small"
+        onChange={handleInp}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Image"
+        variant="outlined"
+        color="grey"
+        name="image"
+        size="small"
+        onChange={handleInp}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Type"
+        variant="outlined"
+        color="grey"
+        name="type"
+        size="small"
+        onChange={handleInp}
+      />
+      <Button
+        variant="contained"
+        size="large"
+        sx={{
+          border: "1px solid black",
+          color: "white",
+          backgroundColor: "black",
+        }}
+      >
+        ADD PRODUCT
+      </Button>
+    </Box>
   );
 };
 
-export default AddBlog;
+export default AddProduct;
