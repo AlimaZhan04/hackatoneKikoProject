b46f96dd74ffa46660eb1fdbc0b9163688dbc765
unknown <zalima073@gmail.com>
2023-04-03 12:15:45 +0600

    pulling

--- src/pages/ProductsPage/ProductPage.jsx
+++ src/pages/ProductsPage/ProductPage.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import ProductList from "../../components/Product/ProductList/ProductList";
+import { Grid } from "@mui/material";
+import SideBar from "../../components/Product/SideBar";
 
 const ProductPage = () => {
-  return <div></div>;
+  return (
+    <Grid sx={{ display: "flex" }}>
+      <Grid item md={3}>
+        <SideBar />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ProductPage;
